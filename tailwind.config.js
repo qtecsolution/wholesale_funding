934d1f15c45aa9dd104a615436a43e091489578f
kawsar ahmed <kawsar.qtec@gmail.com>
2024-12-11 11:48:44 -0500

    Added homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         primary: 'hsl(354, 73%, 43%)', 
+        secondary:'hsl(192, 84%, 55%)'
       },
       fontFamily: {
         sans: ['Inter', 'Arial', 'sans-serif'],  
